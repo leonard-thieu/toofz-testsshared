398a737765d467a10ce25bc7ab3f0b03d3c4df4c
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-04 17:44:48 -0400

    Bump version.

--- toofz.TestsShared/Properties/AssemblyInfo.cs
+++ toofz.TestsShared/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("toofz.TestsShared")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
