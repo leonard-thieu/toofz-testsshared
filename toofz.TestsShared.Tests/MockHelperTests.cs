6d7aca8616ce1232985c3912c6a026fb92c1c5ef
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-07 13:05:45 -0400

    Add params overload.

---
+++ toofz.TestsShared.Tests/MockHelperTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace toofz.TestsShared.Tests
+{
+    class MockHelperTests
+    {
+        [TestClass]
+        public class MockSet
+        {
+            [TestMethod]
+            public void DataIsNull_ThrowsArgumentNullException()
+            {
+                // Arrange
+                IEnumerable<object> data = null;
+
+                // Act
+                var ex = Record.Exception(() =>
+                {
+                    MockHelper.MockSet(data);
+                });
+
+                // Assert
+                Assert.IsInstanceOfType(ex, typeof(ArgumentNullException));
+            }
+
+            [TestMethod]
+            public void DataIsNotNull_ReturnsMockDbSet()
+            {
+                // Arrange
+                IEnumerable<object> data = new[]
+                {
+                    new object(),
+                };
+
+                // Act
+                var mockDbSet = MockHelper.MockSet(data);
+
+                // Assert
+                Assert.IsInstanceOfType(mockDbSet, typeof(Mock<DbSet<object>>));
+            }
+
+            [TestMethod]
+            public void NoParams_ReturnsMockDbSet()
+            {
+                // Arrange -> Act
+                var mockDbSet = MockHelper.MockSet<object>();
+
+                // Assert
+                Assert.IsInstanceOfType(mockDbSet, typeof(Mock<DbSet<object>>));
+            }
+
+            [TestMethod]
+            public void EntitiesIsNotNull_ReturnsMockDbSet()
+            {
+                // Arrange
+                var entity1 = new object();
+                var entity2 = new object();
+
+                // Act
+                var mockDbSet = MockHelper.MockSet(entity1, entity2);
+
+                // Assert
+                Assert.IsInstanceOfType(mockDbSet, typeof(Mock<DbSet<object>>));
+            }
+        }
+    }
+}
