2fde9d94f2b3e63add7a582db618a51f574e9d03
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-10 08:34:07 -0400

    Add asserter that normalizes newlines before comparing strings.

---
+++ toofz.TestsShared/AssertHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace toofz.TestsShared
+{
+    public static class AssertHelper
+    {
+        public static void NormalizedAreEqual(string expected, string actual)
+        {
+            if (expected != null && actual != null)
+            {
+                expected = Regex.Replace(expected, "\r?\n", "\r\n");
+                actual = Regex.Replace(actual, "\r?\n", "\r\n");
+            }
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
