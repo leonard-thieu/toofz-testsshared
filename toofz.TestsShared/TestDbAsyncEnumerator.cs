d4fabaad7ec5d6682149c642d0cad304800baf06
Leonard Thieu <leonard.thieu@gmail.com>
2017-09-05 16:54:51 -0400

    One top-level type per file.

---
+++ toofz.TestsShared/TestDbAsyncEnumerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace toofz.TestsShared
+{
+    sealed class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        public TestDbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            this.inner = inner;
+        }
+
+        readonly IEnumerator<T> inner;
+
+        public T Current => inner.Current;
+        object IDbAsyncEnumerator.Current => Current;
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken) => Task.FromResult(inner.MoveNext());
+
+        public void Dispose() => inner.Dispose();
+    }
+}
---
+++ toofz.TestsShared/TestDbAsyncQueryProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace toofz.TestsShared
+{
+    sealed class TestDbAsyncQueryProvider<TEntity> : IDbAsyncQueryProvider
+    {
+        internal TestDbAsyncQueryProvider(IQueryProvider inner)
+        {
+            this.inner = inner;
+        }
+
+        readonly IQueryProvider inner;
+
+        public IQueryable CreateQuery(Expression expression) => new TestDbAsyncEnumerable<TEntity>(expression);
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression) => new TestDbAsyncEnumerable<TElement>(expression);
+
+        public object Execute(Expression expression) => inner.Execute(expression);
+
+        public TResult Execute<TResult>(Expression expression) => inner.Execute<TResult>(expression);
+
+        public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken) => Task.FromResult(Execute(expression));
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken) => Task.FromResult(Execute<TResult>(expression));
+
+        sealed class TestDbAsyncEnumerable<T> : EnumerableQuery<T>, IDbAsyncEnumerable<T>, IQueryable<T>
+        {
+            public TestDbAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
+
+            public TestDbAsyncEnumerable(Expression expression) : base(expression) { }
+
+            public IDbAsyncEnumerator<T> GetAsyncEnumerator() => new TestDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+
+            IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator() => GetAsyncEnumerator();
+
+            IQueryProvider IQueryable.Provider => new TestDbAsyncQueryProvider<T>(this);
+        }
+    }
+}
